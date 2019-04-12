49a49608abbe3833260d3f333d04f3f403dbcfe5
visualstudio <visualstudio@122-PC03>
2019-04-12 18:23:08 -0400

    commit1

--- classassignment5/Program.cs
+++ classassignment5/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace classassignment5
 {
-    class Program
+    class Program2
     {
-        static void Main(string[] args)
+        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public static void Main()
         {
+
+            Program2 orange = new Program2();
+            Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
         }
     }
 }
